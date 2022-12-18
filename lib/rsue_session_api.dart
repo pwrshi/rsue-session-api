7d523a9aab9204f60f78520834cc56b881063eff
pwrshi <stasonukesweet@gmail.com>
2022-12-19 00:34:28 +0300

    descibe interface

--- lib/rsue_session_api.dart
+++ lib/rsue_session_api.dart
@@ -1,8 +1,18 @@
-
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:rsue_session_api/api.dart';
 import 'rsue_session_api_platform_interface.dart';
 
 class RsueSessionApi {
-  Future<String?> getPlatformVersion() {
+  Future<ScheduleOfExam?> getScheduleOfExams() async {
+    var resp = await Dio().get<ResponseBody>(
+        "https://rsue.ru/studentam/rasp-ekz-of/doc/3/2%20%D0%BA%D1%83%D1%80%D1%81.xls",
+        options: Options(responseType: ResponseType.stream));
+
+    return null;
+  }
+
+  Future<String?> getPlatformVersion() async {
     return RsueSessionApiPlatform.instance.getPlatformVersion();
   }
 }

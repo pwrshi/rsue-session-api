30f20e571a97e6d7f7d06b2feafbfbe05a3a0090
tashinuke <stasonukesweet@gmail.com>
2023-02-18 03:27:55 +0300

    replace useragent

--- lib/rsue_session_api.dart
+++ lib/rsue_session_api.dart
@@ -6,7 +6,11 @@ import 'package:rsue_session_api/api.dart';
 
 class RsueSessionApi {
   Future<List<GroupSchedule?>> getAllSchedules() async {
-    var data = (await Dio().get("https://rsue.ru/studentam/rasp-ekz-of/"))
+    var data = (await Dio().get("https://rsue.ru/studentam/rasp-ekz-of/",
+            options: Options(headers: {
+              "user-agent":
+                  "Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"
+            })))
         .data
         .toString();
     var parseddata = parse(data);
